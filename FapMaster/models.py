742908e27d9fad45be982565250a5c7997417271
RoyCoya <larysing@outlook.com>
2023-09-28 11:47:27 +0800

    [feat] FapMaster static pages and assets

--- FapMaster/models.py
+++ FapMaster/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import settings
 
-# Create your models here.
+# class FapLog(models.Model):
+#     """The records of masturbation
+
+#     # Fields
+#     id: auto increase
+#     user: the one record belongs to
+#     start_time: auto calculated by end_time and duration
+#     end_time: defalt datetime.now (fetch in frontend, since you might rest for a while before you record your job)
+#     duration: timedelta type
+#     comments: max length 200
+#     """
+#     id = models.AutoField(primary_key=True, verbose_name='记录id')
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,1 verbose_name='所属用户')
+#     start_time = models.DateTimeField(null=True, blank=True, verbose_name='开始时间')
+#     end_time = models.DateTimeField(verbose_name='结束时间')
+#     duration = models.DurationField(null=True, blank=True, verbose_name='持续时间')
+#     comments = models.CharField(max_length=200, verbose_name='备注')
--- FapMaster/page/homepage.py
+++ FapMaster/page/homepage.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 # 主页
+@login_required(login_url=settings.LOGIN_URL)
 def homepage(request):
-    context = {
-        
-    }
-    return render(request, 'FapMaster/homepage/homepage.html', context)
+    context = {}
+    return render(request, "FapMaster/homepage/homepage.html", context)
